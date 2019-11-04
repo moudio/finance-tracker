b2668eec1ba31f1d6e2cd23cd23edbc08a871e7e
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-11-04 13:17:40 +0000

    Adding the Friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
 def my_portfolio
 @user = current_user
-@user_stocks = current_user.stocks 
+@user_stocks = current_user.stocks
 end
 
+
+def my_friends
+
+end
 end
