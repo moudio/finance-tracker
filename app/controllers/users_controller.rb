1366f8a0eff85823b4caae00c20f052b1a6bf28d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-11-05 11:51:18 +0000

    Displaying errors for the search friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ end
 
 
 def my_friends
+@friendships = current_user.friends
+end
+
+def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match this search criteria" if @user.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
 
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stocks', to: 'stocks#search'
 get 'my_friends', to: 'users#my_friends'
 resources :user_stocks, only: [:create, :destroy]
+resources :users, only:[:show]
+resources :friendships
+get 'search_friends', to: 'users#search'
 end
